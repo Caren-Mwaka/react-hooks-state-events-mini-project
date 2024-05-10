fbf2af016c4078457152f9e91db940216ffe101b
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-05-10 10:15:20 +0300

    modified all src/components

--- src/__tests__/TaskList.test.js
+++ src/__tests__/TaskList.test.js
@@ -3,6 +3,7 @@ import { render } from "@testing-library/react";
 import TaskList from "../components/TaskList";
 import { TASKS } from "../data";
 
+
 test("displays all items when initially rendered", () => {
   const { container } = render(<TaskList tasks={TASKS} />);
   expect(container.querySelectorAll(".task")).toHaveLength(TASKS.length);
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,21 @@
-import React from "react";
 
-function TaskList() {
+import React from "react";
+import Task from "./Task";
+import { v4 as uuid } from "uuid";
+
+function TaskList({ tasks, setTasks, selectedCategory }) {
+  const tasksChosen = selectedCategory !== 'All' ? tasks.filter(task => task.category === selectedCategory) : tasks;
+
+  const onDelete = taskToDel => {
+    const updatedTasks = tasks.filter(task => task.id !== taskToDel.id);
+    setTasks(updatedTasks);
+  };
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasksChosen.map(task => (
+        <Task onDelete={() => onDelete(task)} task={task} text={task.text} category={task.category} key={uuid()} />
+      ))}
     </div>
   );
 }
